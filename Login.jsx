f57b3e384a18826be5040903708eef696b018867
karthik-sify <karthik.kum.sify@gmail.com>
2023-09-28 14:58:43 +0530

    Task 1

--- Login.jsx
+++ Login.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from 'react';
-import { View, StyleSheet, Text, Image ,Pressable} from 'react-native';
+import { View, StyleSheet, Text, Image, Pressable } from 'react-native';
 
 import UserInputField from './src/Components/UserInputField';
 import Button from './src/Components/Button';
 //import { auth } from '../firebase';
 
-export default function Login({navigation}) {
+export default function Login({ navigation }) {
 
-  const [userEmail, setEmail] = useState('');
-  const [userPassword, setPassword] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
   const photo1 = require("./src/Assests/fb_logo.png");
   const photo2 = require("./src/Assests/google.png");
   const photo3 = require(".//src/Assests/twitter.png");
@@ -22,21 +25,53 @@ export default function Login({navigation}) {
   //   }
   // };
   const handleRegistration = () => {
-    console.log(userEmail + " " + userPassword);
+    if (validateForm()) {
+      // Perform your registration logic here
+      alert('Registration successful!');
   }
+  };
+  function validateForm() {
+    let isValid = true;
+
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(userEmail)) {
+        setEmailError('Invalid email address *');
+        isValid = false;
+    }else setEmailError('')
+    if (userPassword.length < 8) {
+        setPasswordError('Password must be at least 8 characters long *');
+        isValid = false;
+    } else if (!/[a-z]/.test(userPassword) || !/[A-Z]/.test(userPassword)) {
+        setPasswordError('Password must contain both uppercase and lowercase letters *');
+        isValid = false;
+    } else if (!/\d/.test(userPassword)) {
+        setPasswordError('Password must contain at least one digit *');
+        isValid = false;
+    } else if (!/[!@#$%^&*]/.test(userPassword)) {
+        setPasswordError('Password must contain at least one special character *');
+        isValid = false;
+    }else setPasswordError('')
+    
+
+    
+
+    return isValid;
+}
 
   return (
     <View style={styles.PageStyle}>
       <Text style={styles.ReactAppText}>React App</Text>
       <View style={styles.LoginPage}>
         <Text style={styles.LoginTextStyle}>Login</Text>
-        <InputField email={userEmail} password={userPassword} setEmail={setEmail} setPassword={setPassword} style1={styles.InputStyle} style2={styles.TextStyle}></InputField>
+        <UserInputField placeholderValue={"Email"} userValue={userEmail} setfuction={setUserEmail} style1={styles.InputStyle} style2={styles.TextStyle}></UserInputField>
+        <Text style={styles.ValidationStyle}>{emailError}</Text>
+        <UserInputField placeholderValue={"Password"} userValue={userPassword} setfuction={setUserPassword} style1={styles.InputStyle} style2={styles.TextStyle}></UserInputField>
+        <Text style={styles.ValidationStyle}>{passwordError}</Text>
         <Text style={styles.TextStyle}>Forgot password?</Text>
         <Button buttonText={styles.ButtonText} onPress={handleRegistration} buttonName={"Login"}></Button>
-        <Pressable onPress={()=>navigation.navigate('Register')}>
-        <Text style={styles.SignUpStyle}>Don't have an account?
-          <Text style={{ color: "#eb6c49" }}> Sign up</Text>
-        </Text>
+        <Pressable onPress={() => navigation.navigate('Register')}>
+          <Text style={styles.SignUpStyle}>Don't have an account?
+            <Text style={{ color: "#eb6c49" }}> Sign up</Text>
+          </Text>
         </Pressable>
         <Text style={styles.OrStyle}>______________________  OR  _______________________</Text>
         <BottomIcons photo1={photo1} photo2={photo2} photo3={photo3}></BottomIcons>
@@ -47,16 +82,16 @@ export default function Login({navigation}) {
 };
 
 const styles = StyleSheet.create({
-  ReactAppText:{color: 'white', fontWeight: '700', fontSize: 30, marginLeft: 15, marginBottom: 50 },
-  LoginTextStyle: { color: "black", fontSize: 20, fontWeight: "600", marginLeft: 20 },
+  ReactAppText: { color: 'white', fontWeight: '700', fontSize: 30, marginLeft: 15, marginBottom: 50 },
+  LoginTextStyle: { color: "black", fontSize: 20, fontWeight: "600", marginLeft: 20, marginTop: 40, marginBottom: 10 },
   PageStyle: { backgroundColor: "#eb6c49", flex: 1, justifyContent: 'flex-end' },
-  LoginPage: { backgroundColor: "white", borderTopLeftRadius: 30, borderTopRightRadius: 30, padding: 5, paddingTop: 50 },
-  InputStyle: { borderWidth: 0.2, color: "black", borderTopColor: "white", borderLeftColor: "white", borderRightColor: "white", marginBottom: 40, marginLeft: 15, marginRight: 15 },
-  TextStyle: { color: 'black', alignSelf: 'flex-start', marginLeft: 15, fontWeight: '600' },
+  LoginPage: { backgroundColor: "white", borderTopLeftRadius: 30, borderTopRightRadius: 30, padding: 5 },
+  InputStyle: { borderWidth: 0.2, color: "black", borderTopColor: "white", borderLeftColor: "white", borderRightColor: "white", marginLeft: 15, marginRight: 15, marginBottom: 10 },
+  TextStyle: { color: 'black', alignSelf: 'flex-start', marginLeft: 15, fontWeight: '600', marginTop: 20 },
   ButtonText: { marginTop: 50, marginBottom: 40, color: 'white', width: '90%', fontSize: 15, fontWeight: '600', backgroundColor: "#eb6c49", padding: 15, alignSelf: "center", borderRadius: 5, textAlign: 'center' },
-  OrStyle:{color: "grey", textAlign: "center", marginBottom: 20},
-  SignUpStyle:{color: 'black', alignSelf: 'flex-start', marginLeft: 15, fontWeight: '600', alignSelf: 'center', marginBottom: 40}
-
+  OrStyle: { color: "grey", textAlign: "center", marginBottom: 20 },
+  SignUpStyle: { color: 'black', alignSelf: 'flex-start', marginLeft: 15, fontWeight: '600', alignSelf: 'center', marginBottom: 40 },
+  ValidationStyle: { color: "red", fontWeight: '700', marginLeft: 20 }
 });
 
 const InputField = ({ userEmail, userPassword, setEmail, setPassword, style1, style2 }) => {
@@ -68,7 +103,7 @@ const InputField = ({ userEmail, userPassword, setEmail, setPassword, style1, st
   );
 };
 
-const BottomIcons = ({photo1,photo2,photo3}) => {
+const BottomIcons = ({ photo1, photo2, photo3 }) => {
   return (
     <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 20 }}>
       <Image source={photo1} style={{ width: 30, height: 30, margin: 20 }}></Image>
--- Regsiter.jsx
+++ Regsiter.jsx
@@ -19,10 +19,71 @@ export default function Register({ navigation }) {
     const [isChecked, setIsChecked] = useState(false);
     const [date, setDate] = useState(new Date());
     const [showDatePicker, setShowDatePicker] = useState(false);
+    const [firstNameError, setFirstNameError] = useState('');
+    const [lastNameError, setLastNameError] = useState('');
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const [conformPasswordError, setConformPasswordError] = useState('');
+    const[matchPasswordError,setMatchPasswordError]=useState('');
 
 
     const handleRegistration = () => {
-        //console.log(userEmail + " " + userPassword);
+        if (validateForm()) {
+            // Perform your registration logic here
+            alert('Registration successful!');
+        }
+    };
+    
+    function validateForm() {
+        let isValid = true;
+
+        if (userFirstName.trim() === '') {
+            setFirstNameError('First Name is required *');
+            isValid = false;
+        } else setFirstNameError('')
+        if (userLastName.trim() === '') {
+            setLastNameError('Last Name is required *');
+            isValid = false;
+        }else setLastNameError('')
+        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(userEmail)) {
+            setEmailError('Invalid email address *');
+            isValid = false;
+        }else setEmailError('')
+        if (userPassword.length < 8) {
+            setPasswordError('Password must be at least 8 characters long *');
+            isValid = false;
+        } else if (!/[a-z]/.test(userPassword) || !/[A-Z]/.test(userPassword)) {
+            setPasswordError('Password must contain both uppercase and lowercase letters *');
+            isValid = false;
+        } else if (!/\d/.test(userPassword)) {
+            setPasswordError('Password must contain at least one digit *');
+            isValid = false;
+        } else if (!/[!@#$%^&*]/.test(userPassword)) {
+            setPasswordError('Password must contain at least one special character *');
+            isValid = false;
+        }else setPasswordError('')
+        if (userConformPassword.length < 8) {
+            setConformPasswordError('Password must be at least 8 characters long *');
+            isValid = false;
+        } else if (!/[a-z]/.test(userConformPassword) || !/[A-Z]/.test(userConformPassword)) {
+            setConformPasswordError('Password must contain both uppercase and lowercase letters *');
+            isValid = false;
+        } else if (!/\d/.test(userConformPassword)) {
+            setConformPasswordError('Password must contain at least one digit *');
+            isValid = false;
+        } else if (!/[!@#$%^&*]/.test(userConformPassword)) {
+            setConformPasswordError('Password must contain at least one special character *');
+            isValid = false;
+        }else setConformPasswordError('')
+
+        if (userConformPassword !== userPassword) {
+            setMatchPasswordError('Passwords do not match *' );
+            isValid = false;
+          } else {
+            setMatchPasswordError('');
+          }
+
+        return isValid;
     }
 
     const handleDateChange = (event, selectedDate) => {
@@ -40,10 +101,16 @@ export default function Register({ navigation }) {
                 <View style={styles.LoginPage}>
                     <Text style={styles.LoginTextStyle}>Register</Text>
                     <UserInputField placeholderValue={"First Name"} userValue={userFirstName} setfuction={setUserFirstName} style1={styles.InputStyle} style2={styles.TextStyle}></UserInputField>
+                    <Text style={styles.ValidationStyle}>{firstNameError}</Text>
                     <UserInputField placeholderValue={"Last Name"} userValue={userLastName} setfuction={setUserLastName} style1={styles.InputStyle} style2={styles.TextStyle}></UserInputField>
+                    <Text style={styles.ValidationStyle}>{lastNameError}</Text>
                     <UserInputField placeholderValue={"Email"} userValue={userEmail} setfuction={setUserEmail} style1={styles.InputStyle} style2={styles.TextStyle}></UserInputField>
+                    <Text style={styles.ValidationStyle}>{emailError}</Text>
                     <UserInputField placeholderValue={"Password"} userValue={userPassword} setfuction={setUserPassword} style1={styles.InputStyle} style2={styles.TextStyle}></UserInputField>
+                    <Text style={styles.ValidationStyle}>{passwordError}</Text>
                     <UserInputField placeholderValue={"Conform Password"} userValue={userConformPassword} setfuction={setUserConformPassword} style1={styles.InputStyle} style2={styles.TextStyle}></UserInputField>
+                    <Text style={styles.ValidationStyle}>{conformPasswordError}</Text>
+                    <Text style={styles.ValidationStyle}>{matchPasswordError}</Text>
                     <RadioButtons selectedValue={selectedValue} setSelectedValue={setSelectedValue}></RadioButtons>
                     <CheckBox title="Check this box" checked={isChecked} onPress={() => setIsChecked(!isChecked)}></CheckBox>
                     <Pressable onPress={() => setShowDatePicker(true)}>
@@ -79,7 +146,8 @@ const styles = StyleSheet.create({
     ButtonText: { marginTop: 50, marginBottom: 40, color: 'white', width: '90%', fontSize: 15, fontWeight: '600', backgroundColor: "#eb6c49", padding: 15, alignSelf: "center", borderRadius: 5, textAlign: 'center' },
     OrStyle: { color: "grey", textAlign: "center", marginBottom: 20 },
     SignUpStyle: { color: 'black', alignSelf: 'flex-start', marginLeft: 15, fontWeight: '600', alignSelf: 'center', marginBottom: 40 },
-    DateTextStyle:{ color: "black", margin: 30, fontSize: 15,fontWeight:"600",borderWidth:1,width:150,padding:5 }
+    DateTextStyle: { color: "black", margin: 30, fontSize: 15, fontWeight: "600", borderWidth: 1, width: 150, padding: 5 },
+    ValidationStyle: { color: "red", fontWeight: '700', marginLeft: 20 }
 
 });
 
@@ -97,5 +165,3 @@ const RadioButtons = ({ selectedValue, setSelectedValue }) => {
         </View>
     );
 };
-
-
